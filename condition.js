c96aee5b39f98b7252a2bdf88f88f5dad08c5d41
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-03-20 22:27:50 +0530

    Add files via upload
    Conditional Statements - 
    The else if Statement : to specify a new condition if the first condition is false.

---
+++ condition.js
@@ -0,0 +1,17 @@
+const prompt= require('prompt-sync')();
+
+var calls = prompt("Enter number of calls : ");
+
+        if(calls <= 100){
+            charge = 100 * .25 ;
+        }
+        else if( calls <=250){
+            charge = (calls - 100)* .50 + 100*.25 ;
+        }
+        else if( calls <= 500 ){
+            charge = (calls -250)* .75 + 150*.50 + 100*.25 ;
+        }
+        else{
+            charge =(calls-500)*1 + 250*.75 + 150*.50 + 100*.25;
+        }
+    console.log("total charge = ", charge);
