215e8e2e84860a4f165c0b5938bc6b5a4d7d7c9c
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-03-26 21:51:29 +0530

    added love score finder

---
+++ lovescore.js
@@ -0,0 +1,8 @@
+const prompt= require('prompt-sync')();
+
+prompt("enter your name : ");
+prompt("enter their name : ");
+
+var lovescore = Math.random()*100;
+lovescore =Math.floor(lovescore)+1;
+console.log("Your LoveScore is " , lovescore ,"%");
