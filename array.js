ff807312561bfaa9467615de3990f7d5f17b0888
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-03-30 13:29:15 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,19 @@
+        //push : to insert value at last
+        //unshift : to insert value at begining
+        //pop : to delete value from the last
+        //shift : to delete value from the begining
+        //splice : add or remove value
+
+
+        let ar=[2,45,3,23,12,45,67,44,71,89]
+        ar.push(200)
+        ar.unshift(100)
+        ar.pop()
+        ar.shift()
+        ar.splice(2,0,200,300,54,64,321)  //in this it add value
+        ar.splice(3,5)       //it remove 5 value from the index 3
+       console.log(ar ,"<br>")
+
+        //show which element is deleted
+        x=ar.pop()
+        console.log(x) 
